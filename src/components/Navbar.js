c4b9833ce6b560c7c89129b391f8996141fcde5a
AbhigyanBafna <101444239+AbhigyanBafna@users.noreply.github.com>
2023-05-09 22:38:36 +0530

    Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,103 @@
+//The one with changing color, border and glassmorphism
+
+import { SocialIcon } from "react-social-icons";
+import { motion } from "framer-motion";
+import Link from "next/link";
+
+export default function Navbar() {
+
+  return (
+    <motion.div 
+        className="sticky top-0 p-5 flex justify-between z-10"
+        transition={{ duration: 1 }}>
+
+        <motion.div
+            initial={{
+                x: -500,
+                opacity: 0,
+                scale: 0.5,
+              }}
+              animate={{
+                x: 0,
+                opacity: 1,
+                scale: 1,
+              }}
+              transition={{
+                duration: 0.6,
+              }}
+              className="flex flex-row items-center pl-4 font-sans text-6xl text-pHighlight"
+        >
+            ab
+        </motion.div>
+
+        <div className="flex flex-row items-center pl-4 font-sans text-4xl text-heroCircles">
+            <Link href={'/about'} className="px-4">
+              about
+            </Link>
+            <Link href={'/projects'} className="px-4">
+              projects
+            </Link>
+            <Link href={'/blog'} className="px-4">
+              blog
+            </Link>
+        </div>
+        
+      {/* Social Icons */}
+      {/* <motion.div
+        initial={{
+          x: -500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 0.6,
+        }}
+        className="flex flex-row items-center"
+      >
+        <SocialIcon
+          url="https://twitter.com/Abhigyan_Bafna"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://twitter.com/Abhigyan_Bafna"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://twitter.com/Abhigyan_Bafna"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+      </motion.div> */}
+
+      {/* Get in touch  */}
+      <motion.div
+        initial={{
+          x: 500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 0.5,
+        }}
+        className="flex flex-row items-center cursor-pointer"
+      >
+        <SocialIcon network="email" fgColor="gray" bgColor="transparent" style={{ height: 90, width: 90 }}/>
+      </motion.div>
+
+      {/* Spacing Div 2 */}
+      {/* <div className="flex-row items-center hidden lg:flex"></div> */}
+    </motion.div>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+
+const myColors = {
+  'background': '#111010',
+  'pText': '#F5F0F6',
+  'pHighlight' : '#FFB938',
+  'sHighlight' : '#0267C1',
+  'heroCircles': '#808080',
+  'cardBg': '#000000',
+};
+
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -11,6 +21,10 @@ module.exports = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: myColors,
+      fontFamily: {
+        'sans': ['Raleway', 'sans-serif'],
       },
     },
   },
