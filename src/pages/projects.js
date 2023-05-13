38f2aaf2a4a1908521b86c618563f12e4fc889cb
AbhigyanBafna <101444239+AbhigyanBafna@users.noreply.github.com>
2023-05-13 19:48:01 +0530

    Useless Removal
    Removed Useless Stuff

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,25 +1,5 @@
 export default function Projects() {
     
-    const Twitter = require('twitter');
-
-    const client = new Twitter({
-    consumer_key: '<your_consumer_key>',
-    consumer_secret: '<your_consumer_secret>',
-    access_token_key: '<your_access_token_key>',
-    access_token_secret: '<your_access_token_secret>'
-    });
-
-    const params = { screen_name: 'your_twitter_handle', count: 200 };
-
-    client.get('statuses/user_timeline', params, function(error, tweets, response) {
-    if (!error) {
-        console.log(`You have ${tweets.length} tweets.`);
-    } else {
-        console.log(error);
-    }
-    });
-
-
     return(
         <div>
             Projects
